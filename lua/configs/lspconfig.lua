a81d394fd42400b611e0796b1a64559f9513270c
Aarush Agarwal <aarushaga@gmail.com>
2024-05-01 16:23:21 -0700

    fixed pyright installation

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls", "clangd", "pyright"}
+local servers = { "html", "cssls", "clangd"}
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
@@ -21,3 +21,68 @@ lspconfig.tsserver.setup {
   on_init = on_init,
   capabilities = capabilities,
 }
+
+lspconfig.basedpyright.setup {
+  on_attach = on_attach,
+  on_init = on_init,
+  capabilities = capabilities,
+  settings = {
+    python = {
+      pythonPath = "/Users/aarushagarwal/miniconda3/envs/MachineLearning/bin/python3",
+      analysis = {
+        autoSearchPaths = true,
+        diagnosticMode = "workspace",
+        useLibraryCodeForTypes = true,
+        reportGeneralTypeIssues = false,
+        typeCheckingMode = "basic",
+        stubPath = "/Users/aarushagarwal/miniconda3/envs/MachineLearning/lib/python3.8/site-packages",
+        reportUnknownArgumentType = false,
+        reportUnknownCallType = false,
+        reportUnknownVariableType = false,
+      },
+    },
+  },
+}
+
+-- lspconfig.pyright.setup {
+--   on_attach = on_attach,
+--   on_init = on_init,
+--   capabilities = capabilities,
+--   settings = {
+--     python = {
+--       pythonPath = "/Users/aarushagarwal/miniconda3/envs/MachineLearning/bin/python3",
+--       analysis = {
+--         autoSearchPaths = true,
+--         diagnosticMode = "workspace",
+--         useLibraryCodeForTypes = true,
+--       },
+--     },
+--   },
+-- }
+
+-- lspconfig.pylsp.setup {
+--   on_attach = on_attach,
+--   on_init = on_init,
+--   capabilities = capabilities,
+--   -- set python installation path to "/Users/aarushagarwal/miniconda3/envs/MachineLearning/bin/python3"
+--   settings = {
+--     pylsp = {
+--       pythonPath = "/Users/aarushagarwal/miniconda3/envs/MachineLearning/bin/python3",
+--       -- set options
+--       plugins = {
+--         pycodestyle = { enabled = false },
+--         pyflakes = { enabled = false },
+--         pylint = { enabled = true },
+--         yapf = { enabled = false },
+--         pyls_isort = { enabled = true },
+--         pyls_mypy = {
+--           enabled = true,
+--           live_mode = false,
+--           configuration = {
+--             mypyArgs = {"--ignore=E501"}
+--           }
+--         }
+--       },
+--     }
+--   }
+-- }
